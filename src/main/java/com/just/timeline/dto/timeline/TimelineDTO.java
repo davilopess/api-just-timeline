563320ba8eea065e93ab0b7a915df27cf402cc38
Danielopes7 <daniel.lopes.amorim@hotmail.com>
2025-08-24 18:23:20 -0300

    [#1] (feat) - timeline list

---
+++ src/main/java/com/just/timeline/dto/timeline/TimelineDTO.java
@@ -0,0 +1,16 @@
+package com.just.timeline.dto.timeline;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TimelineDTO {
+    private Long id;
+    private String title;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
